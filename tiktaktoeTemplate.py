d712e4214f93e7f4d8635ad4a5511d09928d4818
Judith Schramm <judith.schramm@vodafone.de>
2026-06-23 08:55:17 +0200

    TikTakToe

---
+++ tiktaktoeTemplate.py
@@ -0,0 +1,36 @@
+
+
+# Tic-Tac-Toe Game in Python (Two Players)
+
+
+def print_board(board):
+    # TODO
+
+
+def check_winner(board, player):
+    # TODO 
+
+    # Check columns
+    # TODO
+
+    # Check diagonals
+    # TODO
+
+
+def is_full(board):
+    # TODO
+
+
+def get_move(player, board):
+    # TODO
+
+
+def tic_tac_toe():
+    """Main game loop."""
+    # TODO
+        # Switch player
+        #TODO
+
+
+if __name__ == "__main__":
+    tic_tac_toe()
